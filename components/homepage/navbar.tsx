701ba149a0d8ea303c577b7bcf5e56c6559dc8f2
jawad-nsu <jawadurrahim1997@gmail.com>
2023-11-03 18:25:19 -0400

    narvbar skeleton

--- components/homepage/navbar.tsx
+++ components/homepage/navbar.tsx
@@ -1,5 +1,35 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import { navVariants } from '@/lib/motion';
+import styles from '@/styles';
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <motion.nav
+      variants={navVariants}
+      initial='hidden'
+      whileInView='show'
+      className={`${styles.xPaddings} py-8 relative`}
+    >
+      <div className='absolute w-[50%] inset-0 gradient-01'>
+        <div
+          className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
+        >
+          <img
+            src='/search.svg'
+            alt='search'
+            className='w-[24px] h-[24px] object-contain'
+          />
+          <h2 className='font-extrabold text-[24px] leading-[30px] text-white'>
+            Switch Border Solutions
+          </h2>
+        </div>
+      </div>
+      Navbar
+    </motion.nav>
+  );
 };
 
 export default Navbar;
