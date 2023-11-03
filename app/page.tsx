3ff4a64ee943df94ec43ac09f9956cbc7ea4faf2
jawad-nsu <jawadurrahim1997@gmail.com>
2023-11-03 18:26:53 -0400

    remove placeholder with navbar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Navbar from '@/components/homepage/navbar';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>This is landing page</h1>
+    <main className='bg-background-black'>
+      <Navbar />
     </main>
   );
 }
