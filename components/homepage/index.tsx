6322c93c3b6a2331c342c231a43200e597649cc3
jawad-nsu <jawadurrahim1997@gmail.com>
2023-11-06 10:50:31 -0500

    index file to export components from the folder

---
+++ components/homepage/index.tsx
@@ -0,0 +1,23 @@
+import About from './about';
+import Explore from './explore';
+import Feedback from './feedback';
+import Footer from './footer';
+import GetStarted from './get_started';
+import Hero from './hero';
+import Insights from './insights';
+import Navbar from './navbar';
+import WhatsNew from './whats_new';
+import World from './world_section';
+
+export {
+  About,
+  Navbar,
+  Explore,
+  Feedback,
+  Footer,
+  GetStarted,
+  Hero,
+  Insights,
+  WhatsNew,
+  World,
+};
