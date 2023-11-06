0865d4078750a61e7fe2705d3241843e8335bca8
jawad-nsu <jawadurrahim1997@gmail.com>
2023-11-06 16:22:57 -0500

    list of planets to map over

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const explorePlanets = [
+  {
+    id: 'planet-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'planet-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'planet-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'planet-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'planet-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
