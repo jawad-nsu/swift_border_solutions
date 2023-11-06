b18c2333a7b31067d885ca2ab11bd738b72ae5ea
jawad-nsu <jawadurrahim1997@gmail.com>
2023-11-06 16:15:59 -0500

    explore section completed

--- components/homepage/explore.tsx
+++ components/homepage/explore.tsx
@@ -1,5 +1,52 @@
- const Explore = () => {
-  return <div>Explore</div>;
+'use client';
+
+import { useState } from 'react';
+
+import { motion } from 'framer-motion';
+
+import styles from '@/styles';
+
+import { explorePlanets } from '@/constants';
+
+import ExploreCard from '@/components/common/explore_card';
+import { TitleText } from '@/components/common/title_text';
+import { staggerContainer } from '@/lib/motion';
+
+const Explore = () => {
+  const [active, setActive] = useState('planet-2');
+
+  return (
+    <section className={`${styles.paddings}`}>
+      <motion.div
+        variants={staggerContainer}
+        initial='hidden'
+        whileInView='show'
+        viewport={{ once: false, amount: 0.25 }}
+        className={`${styles.innerWidth} mx-auto flex flex-col`}
+      >
+        <TitleText
+          title={
+            <>
+              Choose the world you want <br className='md:block hidden' /> to
+              explore
+            </>
+          }
+          textStyles='text-center'
+        />
+        <div className='mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5'>
+          {explorePlanets.map((planet, index) => (
+            <ExploreCard
+              key={planet.id}
+              {...planet}
+              index={index}
+              active={active}
+              handleClick={setActive}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </section>
+  );
 };
 
 export default Explore;
