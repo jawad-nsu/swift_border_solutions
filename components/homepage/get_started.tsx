7935bc736edc0ed1d2ec1732c367b419e505a635
jawad-nsu <jawadurrahim1997@gmail.com>
2023-11-07 12:11:41 -0500

    get started component

--- components/homepage/get_started.tsx
+++ components/homepage/get_started.tsx
@@ -6,10 +6,12 @@ import { motion } from 'framer-motion';
 import { planetVariants } from './../../lib/motion';
 import { AnimatedText } from '../common/animated_text';
 import { TitleText } from '../common/title_text';
+import { startingFeatures } from '@/constants';
+import StarterSteps from '@/components/homepage/starter_steps';
 
 const GetStarted = () => {
   return (
-    <section className={`${styles.paddings} relative z-10`}>
+    <section className={`${styles.paddings} mx-5 md:mx-24 relative z-10`}>
       <motion.div
         variants={staggerContainer}
         initial='hidden'
@@ -28,13 +30,26 @@ const GetStarted = () => {
             className='w-[90%] h-[90%] object-contain'
           />
         </motion.div>
+
         {/* text */}
         <motion.div
           variants={fadeIn('left', 'tween', 0.2, 1)}
-          className='flex-[0.75] flex justify-center flex-col'
+          className='flex-[.85] flex justify-center flex-col'
         >
           <AnimatedText text={['| How PlanetPilot Works']} repeatDelay={100} />
-          <TitleText title={<>Get Started with a few clicks</>} />
+          <TitleText
+            title={<>Get Started with just a few clicks</>}
+            textStyles='max-w-[35rem]'
+          />
+          <div className='mt-[31px] flex flex-col max-w-[370px] gap-[24px]'>
+            {startingFeatures.map((feature, index) => (
+              <StarterSteps
+                key={feature}
+                number={`${index < 10 ? '0' : ''} ${index + 1}`}
+                text={feature}
+              />
+            ))}
+          </div>
         </motion.div>
       </motion.div>
     </section>
