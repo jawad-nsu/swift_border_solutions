7d43e0b1661ef85ee49338d8e81c682023cb2fb7
jawad-nsu <jawadurrahim1997@gmail.com>
2024-04-25 11:33:45 -0400

    ts config

--- config.ts
+++ config.ts
@@ -2,5 +2,5 @@ export const ENV = process.env.REACT_APP_ENV;
 export const isDevelopment = ENV === 'DEVELOPMENT';
 export const isStaging = ENV === 'STAGING';
 export const isProduction = ENV === 'PRODUCTION';
-export const BASE_URL = `https://www.alphavantage.co`;
+export const BASE_URL = `https://api.nasa.gov`;
 export const alpha_api_key = process.env.ALPHA_VANTAGE_API_KEY;
